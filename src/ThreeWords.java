9a8ce664243b47ed3c20705215f8db5454c718dc
lanouri <sv.rogozhina@gmail.com>
2024-06-06 20:35:31 +0300

    delete 13 classes

--- src/ThreeWords.java
+++
@@ -1,7 +0,0 @@
-public class ThreeWords {
-        public static void main (String [] args) {
-            System.out.println("Orange");
-            System.out.println("Banana");
-            System.out.println("Apple");
-        }
-    }
