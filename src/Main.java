d35a1c45cb31740b620244c592da9a98942d8211
lanouri <sv.rogozhina@gmail.com>
2024-07-02 17:43:14 +0300

    add class Main

--- src/Main.java
+++ src/Main.java
@@ -1,135 +1,49 @@
-interface Shape {
-    double getPerimeter();
-    double getArea();
-    String getFillColor();
-    String getBorderColor();
-}
-
-class Circle implements Shape {
-    private double radius;
-    private String fillColor;
-    private String borderColor;
-
-    public Circle(double radius, String fillColor, String borderColor) {
-        this.radius = radius;
-        this.fillColor = fillColor;
-        this.borderColor = borderColor;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 2 * Math.PI * radius;
-    }
-
-    @Override
-    public double getArea() {
-        return Math.PI * radius * radius;
-    }
-
-    @Override
-    public String getFillColor() {
-        return fillColor;
-    }
-
-    @Override
-    public String getBorderColor() {
-        return borderColor;
-    }
-}
-
-class Rectangle implements Shape {
-    private double width;
-    private double height;
-    private String fillColor;
-    private String borderColor;
-
-    public Rectangle(double width, double height, String fillColor, String borderColor) {
-        this.width = width;
-        this.height = height;
-        this.fillColor = fillColor;
-        this.borderColor = borderColor;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 2 * (width + height);
-    }
-
-    @Override
-    public double getArea() {
-        return width * height;
-    }
-
-    @Override
-    public String getFillColor() {
-        return fillColor;
-    }
-
-    @Override
-    public String getBorderColor() {
-        return borderColor;
-    }
-}
-
-class Triangle implements Shape {
-    private double side1;
-    private double side2;
-    private double side3;
-    private String fillColor;
-    private String borderColor;
-
-    public Triangle(double side1, double side2, double side3, String fillColor, String borderColor) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-        this.fillColor = fillColor;
-        this.borderColor = borderColor;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return side1 + side2 + side3;
-    }
-
-    @Override
-    public double getArea() {
-        double p = getPerimeter() / 2;
-        return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
-    }
-
-    @Override
-    public String getFillColor() {
-        return fillColor;
-    }
-
-    @Override
-    public String getBorderColor() {
-        return borderColor;
-    }
-}
-
 public class Main {
+
     public static void main(String[] args) {
-        Circle circle = new Circle(5, "Red", "Black");
-        Rectangle rectangle = new Rectangle(3, 4, "Blue", "Green");
-        Triangle triangle = new Triangle(3, 4, 5, "Yellow", "Purple");
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
 
-        System.out.println("Circle:");
-        System.out.println("Perimeter: " + circle.getPerimeter());
-        System.out.println("Area: " + circle.getArea());
-        System.out.println("Fill Color: " + circle.getFillColor());
-        System.out.println("Border Color: " + circle.getBorderColor());
+        try {
+            int sum = sumArrayElements(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
 
-        System.out.println("\nRectangle:");
-        System.out.println("Perimeter: " + rectangle.getPerimeter());
-        System.out.println("Area: " + rectangle.getArea());
-        System.out.println("Fill Color: " + rectangle.getFillColor());
-        System.out.println("Border Color: " + rectangle.getBorderColor());
+    public static int sumArrayElements(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Неверный размер массива");
+        }
 
-        System.out.println("\nTriangle:");
-        System.out.println("Perimeter: " + triangle.getPerimeter());
-        System.out.println("Area: " + triangle.getArea());
-        System.out.println("Fill Color: " + triangle.getFillColor());
-        System.out.println("Border Color: " + triangle.getBorderColor());
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке: [" + i + "][" + j + "]");
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
