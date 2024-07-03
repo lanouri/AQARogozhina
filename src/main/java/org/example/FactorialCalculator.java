54f162cc2f52f0afc2e7876838f5b3371f2fec83
lanouri <sv.rogozhina@gmail.com>
2024-07-03 21:12:13 +0300

    git add

--- src/main/java/org/example/FactorialCalculator.java
+++ src/main/java/org/example/FactorialCalculator.java
@@ -7,6 +7,7 @@ public class FactorialCalculator {
         }
     }
 
+
     public static void main(String[] args) {
         FactorialCalculator calculator = new FactorialCalculator();
         int number = 5;
